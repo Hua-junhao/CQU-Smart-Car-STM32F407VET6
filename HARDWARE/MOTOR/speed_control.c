e7606dab856342e2c4a877c3a259206250e602e8
Hua-junhao <hua1249639696@gmail.com>
2026-07-10 13:25:27 +0800

    feat(robot): release finalized integrated line-tracking and obstacle avoidance system
    - Integrate 8-channel infrared sensor fusion with ultrasonic obstacle-avoidance logic.
    
    - Stabilize 4-way motor speed control via fine-tuned positional PID closed loop.
    
    - Resolve state priority conflicts, enabling smooth S-curves and U-turn navigation.
    
    - Conclude the CQU Mobile Robot summer course with a fully functional, autonomous vehicle.

--- HARDWARE/MOTOR/speed_control.c
+++ HARDWARE/MOTOR/speed_control.c
@@ -3,11 +3,12 @@
 #include "motor.h"
 #include <stdlib.h> 
 
-#define TRIM_M1  0   
-#define TRIM_M2  0   
-#define TRIM_M3  0   
-#define TRIM_M4  0   
+#define TRIM_M1   0   
+#define TRIM_M2   0   
+#define TRIM_M3   0   
+#define TRIM_M4   0   
 
+/* ???????????? */
 #define PID_KP  9  
 #define PID_KI  3   
 #define PID_KD  0  
@@ -15,74 +16,87 @@
 typedef struct { int16_t integral; int16_t last_error; } PID_T;
 static PID_T g_pid[4];
 static volatile uint8_t g_speed_ctrl_enable = 0;
-static volatile int16_t g_target_l = 0, g_target_r = 0;
+static volatile int16_t g_target_left = 0, g_target_right = 0;
+static volatile int16_t g_target_ramp_left = 0, g_target_ramp_right = 0;
 static volatile uint16_t g_motor_duty[4] = {0,0,0,0};
 
 void SPEED_CTRL_Init(void) {
     TIM_TimeBaseInitTypeDef TIM; 
-    // ?? ????:????? NVIC ?????,?? NVIC_Config ??
-    NVIC_InitTypeDef NVIC_Config; 
-    
+    NVIC_InitTypeDef NVIC_Init_Struct; 
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM7, ENABLE);
-    TIM.TIM_Period = 49999; 
-    TIM.TIM_Prescaler = 84 - 1;
-    TIM.TIM_ClockDivision = TIM_CKD_DIV1;
-    TIM.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBaseInit(TIM7, &TIM);
-
-    TIM_ClearITPendingBit(TIM7, TIM_IT_Update);
+    TIM.TIM_Period = 49999; TIM.TIM_Prescaler = 84-1; TIM_TimeBaseInit(TIM7, &TIM);
     TIM_ITConfig(TIM7, TIM_IT_Update, ENABLE);
-
-    // ?? ?? NVIC_Config ????? NVIC
-    NVIC_Config.NVIC_IRQChannel = TIM7_IRQn; 
-    NVIC_Config.NVIC_IRQChannelPreemptionPriority = 2; 
-    NVIC_Config.NVIC_IRQChannelSubPriority = 0;
-    NVIC_Config.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_Config);
+    NVIC_Init_Struct.NVIC_IRQChannel = TIM7_IRQn; 
+    NVIC_Init_Struct.NVIC_IRQChannelPreemptionPriority = 2; 
+    NVIC_Init_Struct.NVIC_IRQChannelCmd = ENABLE; 
+    NVIC_Init(&NVIC_Init_Struct);
     TIM_Cmd(TIM7, ENABLE);
 }
 
 void SPEED_CTRL_Start(void) { g_speed_ctrl_enable = 1; }
-void SPEED_CTRL_Stop(void)  { g_speed_ctrl_enable = 0; MOTOR_StopAll(); for(int i=0;i<4;i++) g_pid[i].integral=0; }
-
-void SPEED_CTRL_SetSideTargetPulse(int16_t left, int16_t right) { 
-    g_target_l = left; 
-    g_target_r = right; 
-}
-
-int16_t SPEED_CTRL_GetTargetLeft(void) { return g_target_l; }
-int16_t SPEED_CTRL_GetTargetRight(void) { return g_target_r; }
+void SPEED_CTRL_Stop(void)  { g_speed_ctrl_enable = 0; g_target_ramp_left = 0; g_target_ramp_right = 0; MOTOR_StopAll(); for(int i=0;i<4;i++) g_pid[i].integral=0; }
+void SPEED_CTRL_SetSideTargetPulse(int16_t left, int16_t right) { g_target_left = left; g_target_right = right; }
+int16_t SPEED_CTRL_GetTargetLeft(void) { return g_target_left; }
+int16_t SPEED_CTRL_GetTargetRight(void) { return g_target_right; }
 uint16_t SPEED_CTRL_GetDuty(uint8_t motor) { return (motor>=1 && motor<=4) ? g_motor_duty[motor-1] : 0; }
 uint8_t SPEED_CTRL_GetStatus(void) { return g_speed_ctrl_enable; }
 
 void TIM7_IRQHandler(void) {
-    if (TIM_GetITStatus(TIM7, TIM_IT_Update)) {
+    if (TIM_GetITStatus(TIM7, TIM_IT_Update) != RESET) {
         TIM_ClearITPendingBit(TIM7, TIM_IT_Update);
         ENCODER_Update(); 
 
         if (!g_speed_ctrl_enable) { MOTOR_StopAll(); return; }
 
-        for (int i = 0; i < 4; i++) {
-            int16_t target = (i < 2) ? g_target_l : g_target_r;
-            if (i == 0) target += TRIM_M1;
-            if (i == 1) target += TRIM_M2;
-            if (i == 2) target += TRIM_M3;
-            if (i == 3) target += TRIM_M4;
+        /* ????:????,????,???? */
+        if (g_target_ramp_left < g_target_left) g_target_ramp_left += 5; 
+        else if (g_target_ramp_left > g_target_left) g_target_ramp_left -= 5;
+        if (g_target_ramp_right < g_target_right) g_target_ramp_right += 5; 
+        else if (g_target_ramp_right > g_target_right) g_target_ramp_right -= 5;
 
+        for (int i = 0; i < 4; i++) {
+            int16_t my_target = (i == 0 || i == 1) ? g_target_ramp_left : g_target_ramp_right;
+
+            if (i == 0) my_target += TRIM_M1;
+            if (i == 1) my_target += TRIM_M2;
+            if (i == 2) my_target += TRIM_M3;
+            if (i == 3) my_target += TRIM_M4;
+            if (my_target < 0) my_target = 0;
+
+            int16_t target_abs = abs(my_target);
             int16_t measured = ENCODER_GetSpeedPulse(i + 1); 
-            int32_t error = target - measured;
+            int32_t error = target_abs - measured;
             
+            if (error >= -1 && error <= 1) error = 0; 
+
             g_pid[i].integral += error;
-            if (g_pid[i].integral > 80) g_pid[i].integral = 80;   
+            if (g_pid[i].integral > 80)  g_pid[i].integral = 80;   
             if (g_pid[i].integral < -80) g_pid[i].integral = -80;
 
-            int32_t output = 25 + (PID_KP * error + PID_KI * g_pid[i].integral) / 10;
+            /* ?? ???? 1:?????? (Feed-Forward)
+               ???? = 20 + ????*1.2?
+               ???? 20 ??,??????? 44% ????!????????????! */
+            int32_t base_duty = (target_abs == 0) ? 0 : (10 + (target_abs * 12 / 10)); 
 
-            if (output > 70) output = 70; 
+            int32_t output = base_duty + (PID_KP * error + PID_KI * g_pid[i].integral) / 10;
+
+            /* ?? ???? 2:???? (Stiction Override)
+               ???????,???????(???0),?????? 15% ???????!
+               ?????,? 15% ????,????! */
+            if (target_abs > 0 && measured == 0) {
+                output += 15; 
+            }
+
+            /* ??????? 85%,???????????? */
+            if (output > 55) output = 55; 
             if (output < 0)  output = 0;
 
             g_motor_duty[i] = (uint16_t)output;
             MOTOR_SetDuty(i + 1, g_motor_duty[i]);
+            
+            /* ?????????? */
+            if (my_target >= 0) MOTOR_SetDirectionDynamic(i + 1, 0); 
+            else                MOTOR_SetDirectionDynamic(i + 1, 1); 
         }
     }
 }
--- HARDWARE/OBSTACLE/obstacle.c
+++ HARDWARE/OBSTACLE/obstacle.c
@@ -1,43 +1,97 @@
 #include "obstacle.h"
 #include "ultrasonic.h"
+#include "servo.h"
 #include "line_sensor.h"
 #include "speed_control.h"
-#include "servo.h"
 #include "delay.h"
 
-static uint8_t g_state = 0; 
-void OBSTACLE_Init(void) { g_state = 0; }
-const char* OBSTACLE_GetStateString(void) { return (g_state==0)?"TRACK":"AVOID"; }
+/* ================== ?? ??????? ================== */
+#define OBSTACLE_SAFE_CM     22   
+#define SERVO_CENTER_ANGLE   90   
+#define SERVO_LEFT_ANGLE     135  
+#define SERVO_RIGHT_ANGLE    45   
+
+/* ?? ???? 1:??????? 24,???????,????! */
+#define BASE_SPEED           22   
+
+/* ?? ???? 2:????? 4,??? S ??????????,????????! */
+#define TURN_SENSITIVITY     4  			  
+
+/* ???????????? 22,?????????? */
+#define SPIN_SPEED           18   
+
+#define INVERT_TRACKING_DIR  0    
+#define EVADE_TURN_SPEED     25   
+#define OBSTACLE_TURN_MS     800  
+
+typedef enum { STATE_FORWARD=0, STATE_SCAN, STATE_LEFT, STATE_RIGHT, STATE_ESCAPE } OBSTACLE_STATE_T;
+static OBSTACLE_STATE_T g_state = STATE_FORWARD;
+static int16_t last_turn_error = 0; 
+
+void OBSTACLE_Init(void) { g_state = STATE_FORWARD; SERVO_SetAngle(1, SERVO_CENTER_ANGLE); SPEED_CTRL_SetSideTargetPulse(0,0); }
+uint16_t OBSTACLE_GetFrontCm(void) { return ULTRASONIC_GetDistanceFilteredCm(1); }
+uint16_t OBSTACLE_GetLeftCm(void)  { return 0; } 
+uint16_t OBSTACLE_GetRightCm(void) { return 0; }
+const char* OBSTACLE_GetStateString(void) { return (g_state == STATE_FORWARD) ? "TRACKING" : "AVOIDING"; }
+
+static void Process_LineTracking(void) {
+    uint8_t line_data = LINE_ReadAll();
+    int16_t error = 0;
+    int16_t active_sensors = 0;
+
+    for (int i = 0; i < 8; i++) {
+        if (line_data & (1 << i)) {
+            error += (i - 3.5) * 10; 
+            active_sensors++;
+        }
+    }
+
+    if (active_sensors > 0 && active_sensors <= 5) {
+        error = error / active_sensors; 
+        last_turn_error = error; 
+        
+        int16_t turn_val = (error * TURN_SENSITIVITY) / 10;
+        if (INVERT_TRACKING_DIR) turn_val = -turn_val;
+
+        /* ?? ????:?????,???????????,???? S ?! */
+        int16_t left_spd  = BASE_SPEED + turn_val;
+        int16_t right_spd = BASE_SPEED - turn_val;
+        SPEED_CTRL_SetSideTargetPulse(left_spd, right_spd);
+    } 
+    else if (active_sensors > 5) {
+        /* ????:???? */
+        SPEED_CTRL_SetSideTargetPulse(BASE_SPEED, BASE_SPEED);
+    }
+    else {
+        /* ?? ???/???:????,??????????“????”??????! */
+        if (last_turn_error > 0) {
+            SPEED_CTRL_SetSideTargetPulse(SPIN_SPEED, -SPIN_SPEED); 
+        } else {
+            SPEED_CTRL_SetSideTargetPulse(-SPIN_SPEED, SPIN_SPEED); 
+        }
+    }
+}
 
 void OBSTACLE_Task(void) {
+    if (!SPEED_CTRL_GetStatus()) { SERVO_SetAngle(1, SERVO_CENTER_ANGLE); return; }
+
+    SERVO_SetAngle(1, SERVO_CENTER_ANGLE);
     uint16_t dist = ULTRASONIC_GetDistanceFilteredCm(1);
-    
-    // ???????? (22cm??)
-    if(dist < 22 && dist > 0) {
-        g_state = 1;
-        SPEED_CTRL_SetSideTargetPulse(0,0); delay_ms(100);
-        
-        SERVO_SetAngle(1, 135); delay_ms(300); uint16_t l = ULTRASONIC_GetDistanceFilteredCm(1);
-        SERVO_SetAngle(1, 45);  delay_ms(300); uint16_t r = ULTRASONIC_GetDistanceFilteredCm(1);
-        SERVO_SetAngle(1, 90);
-        
-        // ????:????????
-        if(l < r) SPEED_CTRL_SetSideTargetPulse(25, -25); 
-        else      SPEED_CTRL_SetSideTargetPulse(-25, 25);
-        
-        delay_ms(800); // ????
-        g_state = 0;
+
+    if (dist >= OBSTACLE_SAFE_CM || dist == ULTRASONIC_INVALID_CM) {
+        g_state = STATE_FORWARD;
+        Process_LineTracking(); 
     } else {
-        g_state = 0;
-        uint8_t data = LINE_ReadAll();
-        int16_t err = 0; int cnt = 0;
-        for(int i=0; i<8; i++) if(data&(1<<i)) { err += (i-3.5)*5; cnt++; }
-        
-        // ????:??????????
-        if(cnt) {
-             SPEED_CTRL_SetSideTargetPulse(20+err, 20-err);
-        } else {
-             SPEED_CTRL_SetSideTargetPulse(20, 20);
-        }
+        g_state = STATE_SCAN; SPEED_CTRL_SetSideTargetPulse(0, 0); delay_ms(100); 
+        SERVO_SetAngle(1, SERVO_LEFT_ANGLE);  delay_ms(150); uint16_t l = ULTRASONIC_GetDistanceFilteredCm(1);
+        SERVO_SetAngle(1, SERVO_RIGHT_ANGLE); delay_ms(150); uint16_t r = ULTRASONIC_GetDistanceFilteredCm(1);
+        SERVO_SetAngle(1, SERVO_CENTER_ANGLE); delay_ms(50);
+
+        if (l < r) { g_state = STATE_RIGHT; SPEED_CTRL_SetSideTargetPulse(EVADE_TURN_SPEED, -EVADE_TURN_SPEED); } 
+        else       { g_state = STATE_LEFT;  SPEED_CTRL_SetSideTargetPulse(-EVADE_TURN_SPEED, EVADE_TURN_SPEED); }
+
+        g_state = STATE_ESCAPE; delay_ms(OBSTACLE_TURN_MS); 
+        SPEED_CTRL_SetSideTargetPulse(0, 0); delay_ms(50);
+        g_state = STATE_FORWARD;
     }
 }
